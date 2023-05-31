dc1767cc2438ac4d3653150691da548bcf5d3092
dudoof <email@example.com>
2023-05-31 10:24:52 +0200

    first

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+//from here
+// https://doc.rust-lang.org/book/ch12-01-accepting-command-line-arguments.html
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    dbg!(args);
+}
